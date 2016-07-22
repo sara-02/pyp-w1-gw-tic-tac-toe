57770b836a7811fe2c136cd8d0fc9bf74432f4ca
Santiago Basulto <santiago.basulto@gmail.com>
2016-07-22 10:29:06 -0300

    Fixed dependencies for tox

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     packages=['tic_tac_toe'],
     maintainer='rmotr.com',
     tests_require=[
-        'pytest==2.9.1',
+        'pytest==2.9.2',
         'pytest-cov==2.2.1',
         'coverage==4.0.3'
     ],
